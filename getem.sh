ec3680681606454fa8aae66bd93863e0b6df0825
Nick Pettazzoni <github@n.pettazz.com>
2025-01-10 14:49:58 -0500

    more configs

--- getem.sh
+++ getem.sh
@@ -3,6 +3,9 @@
 for f in ".zshrc"\
          ".todo.cfg"\
          ".timewarrior/timewarrior.cfg"\
+         "Library/Application Support/com.mitchellh.ghostty/config"\
+         "Library/Application Support/Sublime Text/Packages/User/Preferences.sublime-settings"\
+         "Library/Application Support/Sublime Text/Packages/User/ayu-mirage.sublime-color-scheme"\
          ".p10k.zsh"; do
   cp "$HOME/$f" .
 done
